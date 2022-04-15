b082ea7ee723c7e9ac1b9bd1007ab3dff921c078
Perry Shelton <sellwithperry@gmail.com>
2022-04-15 09:24:27 -0700

    init

---
+++ src/main/java/com/perbear/kidwrangler/KidWranglerApplication.java
@@ -0,0 +1,13 @@
+package com.perbear.kidwrangler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidWranglerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidWranglerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perbear/kidwrangler/KidWranglerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perbear.kidwrangler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidWranglerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
