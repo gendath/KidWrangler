e464858834b1bf10fbb6df8e67114d4f7bcc0bb3
gendath <sellwithperry@gmail.com>
2022-05-04 12:06:45 -0700

    Add Index Page and Controller closes #8

---
+++ KidWranglerWeb/src/main/java/com/perbear/kidwrangler/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.perbear.kidwrangler.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String home(Model model){
+        return "index";
+    }
+}
