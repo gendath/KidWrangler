da6c239491ac7742ff760ce6f63561170089e063
gendath <sellwithperry@gmail.com>
2022-07-11 08:34:11 -0700

    create specialty  entity and added to doctor entity closes #32

--- KidWranglerData/src/main/java/com/perbear/kidwrangler/Model/Doctor.java
+++ KidWranglerData/src/main/java/com/perbear/kidwrangler/Model/Doctor.java
@@ -1,7 +1,9 @@
 package com.perbear.kidwrangler.Model;
 
 import java.util.List;
+import java.util.Set;
 
 public class Doctor extends Person{
     private List<Patient> patients;
+    private Set<Specialty> specialties;
 }
