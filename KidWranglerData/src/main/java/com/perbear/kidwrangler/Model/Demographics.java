349d2c9bf68ec9bdfbc27ecba1b8f5e03572987a
gendath <sellwithperry@gmail.com>
2022-07-11 08:20:14 -0700

    create demographics entity closes #43

---
+++ KidWranglerData/src/main/java/com/perbear/kidwrangler/Model/Demographics.java
@@ -0,0 +1,7 @@
+package com.perbear.kidwrangler.Model;
+
+public class Demographics extends BaseEntity{
+    private String gender;
+    private String race;
+    private String sexualOrientation;
+}
